ae22a772efad0e025c60a7d87b447937cb2ed244
Aleksandr Chernyaev <Aleksandr>
2024-01-31 18:43:47 +0300

    add task4

---
+++ Task_4/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Задача 4: Напишите программу, которая на вход
+принимает натуральное число N, а на выходе
+показывает его цифры через запятую.*/
+
+int num = 568;
+int ten = 1;
+
+while (ten < num / 10)
+{
+    ten = ten * 10;
+}
+while (ten > 0)
+{
+    int res = num / ten;
+    num = num % ten;
+    ten = ten / 10;
+    System.Console.Write(res);
+
+    if (ten >= 1)
+    System.Console.Write(",");
+}
